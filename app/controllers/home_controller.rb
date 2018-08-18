13e74d3b7068bf05850417074ea922d690f456ef
ItinoseSan <jkekun@gmail.com>
2018-08-18 10:04:20 +0900

    initial-commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def index
+    @message = "一応トップページ部分"
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:show, :new, :edit, :create, :update]
+  resources :sessions, only: [:new, :create, :destroy]
+
+  match 'signup', to: 'users#new', via: 'get'
+  match 'signin', to: 'sessions#new', via: 'get'
+  match 'signout', to: 'sessions#destroy', via: 'delete'
+
+  root 'home#index'
+end
